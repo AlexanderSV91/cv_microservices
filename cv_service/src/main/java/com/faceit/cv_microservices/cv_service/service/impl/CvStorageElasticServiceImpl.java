0c4ac8c9ebb529444eeaa6b9f503a35ec5cc4223
AlexanderSV91 <alex.shalamov4@gmail.com>
2021-05-27 17:31:13 +0300

    add fallback method. Resilience4

--- cv_service/src/main/java/com/faceit/cv_microservices/cv_service/service/impl/CvStorageElasticServiceImpl.java
+++ cv_service/src/main/java/com/faceit/cv_microservices/cv_service/service/impl/CvStorageElasticServiceImpl.java
@@ -3,13 +3,21 @@ package com.faceit.cv_microservices.cv_service.service.impl;
 import com.faceit.cv_microservices.cv_service.dto.response.CvResponse;
 import com.faceit.cv_microservices.cv_service.mapper.CvElasticMapper;
 import com.faceit.cv_microservices.cv_service.model.elastic.CvElastic;
+import com.faceit.cv_microservices.cv_service.model.elastic.PreviousWorkElastic;
+import com.faceit.cv_microservices.cv_service.model.elastic.SalaryElastic;
+import com.faceit.cv_microservices.cv_service.model.elastic.UserElastic;
 import com.faceit.cv_microservices.cv_service.service.CvStorageElasticService;
 import com.faceit.cv_microservices.cv_service.service.CvStorageServiceFeignClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 
 @Log4j2
 @Service
@@ -24,6 +32,8 @@ public class CvStorageElasticServiceImpl implements CvStorageElasticService {
         this.cvElasticMapper = cvElasticMapper;
     }
 
+    @Retry(name = "cv-elastic-resilience", fallbackMethod = "findAllCvFallback")
+    @CircuitBreaker(name = "cv-elastic-resilience")
     @Override
     public Page<CvResponse> findAllCv(Pageable pageable) {
         Page<CvElastic> cvPage = this.cvStorageServiceFeignClient.findAllCvElastic(pageable);
@@ -32,5 +42,19 @@ public class CvStorageElasticServiceImpl implements CvStorageElasticService {
 
     private PageImpl<CvResponse> pageEntityToPageResponse(Page<CvElastic> page) {
         return new PageImpl<>(this.cvElasticMapper.toCvResponseList(page.getContent()), page.getPageable(), page.getTotalElements());
+    }
+
+    private Page<CvElastic> findAllCvFallback(final Pageable pageable, final Exception ex) {
+        CvElastic cvElastic = new CvElastic(
+                "-1",
+                "no",
+                new SalaryElastic("no", -1),
+                new UserElastic("no", "no", "no"),
+                Collections.singletonList("no"),
+                "no",
+                Collections.singletonList(new PreviousWorkElastic("no", "no", "-1")),
+                "no");
+        List<CvElastic> list = Collections.singletonList(cvElastic);
+        return new PageImpl<>(list, pageable, 0);
     }
 }
--- cv_service/src/main/java/com/faceit/cv_microservices/cv_service/service/impl/CvStorageMongoServiceImpl.java
+++ cv_service/src/main/java/com/faceit/cv_microservices/cv_service/service/impl/CvStorageMongoServiceImpl.java
@@ -2,14 +2,23 @@ package com.faceit.cv_microservices.cv_service.service.impl;
 
 import com.faceit.cv_microservices.cv_service.dto.response.CvResponse;
 import com.faceit.cv_microservices.cv_service.mapper.CvMongoMapper;
+import com.faceit.cv_microservices.cv_service.model.elastic.CvElastic;
+import com.faceit.cv_microservices.cv_service.model.elastic.PreviousWorkElastic;
+import com.faceit.cv_microservices.cv_service.model.elastic.SalaryElastic;
+import com.faceit.cv_microservices.cv_service.model.elastic.UserElastic;
 import com.faceit.cv_microservices.cv_service.model.mongo.CvMongo;
 import com.faceit.cv_microservices.cv_service.service.CvStorageMongoService;
 import com.faceit.cv_microservices.cv_service.service.CvStorageServiceFeignClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 
 @Log4j2
 @Service
@@ -24,6 +33,8 @@ public class CvStorageMongoServiceImpl implements CvStorageMongoService {
         this.cvMongoMapper = cvMongoMapper;
     }
 
+    @Retry(name = "cv-mongo-resilience", fallbackMethod = "findAllCvFallback")
+    @CircuitBreaker(name = "cv-mongo-resilience")
     @Override
     public Page<CvResponse> findAllCv(Pageable pageable) {
         Page<CvMongo> cvPage = this.cvStorageServiceFeignClient.findAllCvMongo(pageable);
@@ -32,5 +43,19 @@ public class CvStorageMongoServiceImpl implements CvStorageMongoService {
 
     private PageImpl<CvResponse> pageEntityToPageResponse(Page<CvMongo> page) {
         return new PageImpl<>(this.cvMongoMapper.toCvResponseList(page.getContent()), page.getPageable(), page.getTotalElements());
+    }
+
+    private Page<CvElastic> findAllCvFallback(final Pageable pageable, final Exception ex) {
+        CvElastic cvElastic = new CvElastic(
+                "-1",
+                "no",
+                new SalaryElastic("no", -1),
+                new UserElastic("no", "no", "no"),
+                Collections.singletonList("no"),
+                "no",
+                Collections.singletonList(new PreviousWorkElastic("no", "no", "-1")),
+                "no");
+        List<CvElastic> list = Collections.singletonList(cvElastic);
+        return new PageImpl<>(list, pageable, 0);
     }
 }
