729a506e9b0295d2f44ce48f77e1e56f78a7e41b
AlexanderSV91 <alex.shalamov4@gmail.com>
2021-05-31 13:49:20 +0300

    init auth_service

---
+++ auth_service/src/main/java/com/faceit/cv_microservices/auth_service/AuthServiceApplication.java
@@ -0,0 +1,16 @@
+package com.faceit.cv_microservices.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableEurekaClient
+@EnableFeignClients
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+}
